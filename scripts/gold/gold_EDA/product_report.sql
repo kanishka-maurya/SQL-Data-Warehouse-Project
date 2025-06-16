4f72b8bdf30cadc56c66452c3704fc4779639a31
KΛИIƧHҜΛ <166901234+kanishka-maurya@users.noreply.github.com>
2025-06-16 12:22:22 +0530

    Update product_report.sql

--- scripts/gold/gold_EDA/product_report.sql
+++ scripts/gold/gold_EDA/product_report.sql
@@ -34,9 +34,9 @@ with base_query as (
 1) base query: retrieves core columns from fact_sales and dim_products
 ---------------------------------------------------------------------------*/
     select
-	    f.order_number,
+	f.order_number,
         f.order_date,
-		f.customer_key,
+	f.customer_key,
         f.sales_amount,
         f.quantity,
         p.product_key,
@@ -46,7 +46,7 @@ with base_query as (
         p.cost
     from gold.fact_sales f
     left join gold.dim_products p
-        on f.product_key = p.product_key
+    on f.product_key = p.product_key
     where order_date is not null  -- only consider valid sales dates
 ),
 
@@ -63,12 +63,11 @@ select
     datediff(month, min(order_date), max(order_date)) as lifespan,
     max(order_date) as last_sale_date,
     count(distinct order_number) as total_orders,
-	count(distinct customer_key) as total_customers,
+    count(distinct customer_key) as total_customers,
     sum(sales_amount) as total_sales,
     sum(quantity) as total_quantity,
-	round(avg(cast(sales_amount as float) / nullif(quantity, 0)),1) as avg_selling_price
+    round(avg(cast(sales_amount as float) / nullif(quantity, 0)),1) as avg_selling_price
 from base_query
-
 group by
     product_key,
     product_name,
@@ -89,9 +88,9 @@ select
 	last_sale_date,
 	datediff(month, last_sale_date, getdate()) as recency_in_months,
 	case
-		when total_sales > 50000 then 'high-performer'
-		when total_sales >= 10000 then 'mid-range'
-		else 'low-performer'
+	    when total_sales > 50000 then 'high-performer'
+	    when total_sales >= 10000 then 'mid-range'
+	    else 'low-performer'
 	end as product_segment,
 	lifespan,
 	total_orders,
@@ -101,14 +100,14 @@ select
 	avg_selling_price,
 	-- average order revenue (aor)
 	case 
-		when total_orders = 0 then 0
-		else total_sales / total_orders
+	    when total_orders = 0 then 0
+	    else total_sales / total_orders
 	end as avg_order_revenue,
 
 	-- average monthly revenue
 	case
-		when lifespan = 0 then total_sales
-		else total_sales / lifespan
+	    when lifespan = 0 then total_sales
+	    else total_sales / lifespan
 	end as avg_monthly_revenue
 
 from product_aggregations;
